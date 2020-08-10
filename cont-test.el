f1b0622b0f97cc93220b5cc8b336326cb9b53f98
Yilin Wei <yilin.wei10@gmail.com>
2020-08-10 23:54:48 +0100

    Add yield control flow operator

--- cont-test.el
+++ cont-test.el
@@ -4,3 +4,11 @@
   (should
    (=
     (reset (+ 3 (shift k (k 2)) 1)) 6)))
+
+(ert-deftest yield-simple-test ()
+  (should
+   (equal
+    (reset
+     (yield 1)
+     (yield 2))
+    '(1 2))))
--- cont.el
+++ cont.el
@@ -2,32 +2,45 @@
 
 (setq lexical-binding t)
 
+(cl-defmacro yield (x)
+  `(shift k (cons ,x (k nil))))
+
+(cl-defmacro shift (id expr)
+  "The `shift' control flow operator for delimited continuations. See `reset'."
+  `(user-error "`shift' operator should be rewritten out, call in `reset' block."))
+
 (cl-defmacro reset (expr &rest exprs)
+  "Create a delimited continuation, rewriting control flow operators inside (EXPR . EXPRS)."
   (letrec
       ((rewrite
 	;; `ret' is an early return
 	(lambda (ret expr)
 	  (pcase expr
+	    ;;TODO: generalize control flow operators?
+	    (`(yield ,expr)
+	     (funcall rewrite ret (macroexpand-1 `(yield ,expr))))
 	    (`(shift ,(and id (pred symbolp sym)) ,expr)
 	     (let*
 		 ((sym (gensym))
 		  (subst (funcall ret sym)))
 	       `(cl-flet
-		    ((,id (lambda (,sym) ,subst)))
+		    ((,id (lambda (,sym) ,subst))) 
 		  ,expr)))
 	    (`(,expr . ,exprs)
-	     ;; redex: [E/v] -> 
+	     ;; redex: [E/v] ->
 	     (funcall rewrite
-		      (lambda (t)
+		      (lambda (h)
 			(funcall rewrite
-				 (lambda (h)
+				 (lambda (t)
 				   (funcall ret (cons h t)))
-				 expr))
-		      exprs))
+				 exprs))
+		      expr))
 	    ;; redex : VAL -> VAL
 	    (_ (funcall ret expr)))
 	  )
 	))
-    (funcall rewrite #'identity expr)))
+    (if exprs
+	`(reset (progn ,expr ,@exprs))
+      (funcall rewrite #'identity expr))))
 
 (provide 'cont)
